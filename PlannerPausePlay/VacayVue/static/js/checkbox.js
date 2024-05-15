25ed7dc478e536acd1300f191f21ec08950cc627
ElliePs6 <eleuthpayne@gmail.com>
2024-05-15 10:48:13 +0300

    suck

---
+++ PlannerPausePlay/VacayVue/static/js/checkbox.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    // Add event listener to each checkbox
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('click', function() {
+            // Uncheck all checkboxes except the one that was clicked
+            checkboxes.forEach(cb => {
+                if (cb !== checkbox) {
+                    cb.checked = false;
+                }
+            });
+        });
+    }); 
+})
---
+++ PlannerPausePlay/VacayVue/static/js/custom-greek.js
@@ -0,0 +1,24 @@
+// custom-greek.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const calendarEl = document.getElementById('calendar');
+
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+        locale: 'el',
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+            left: 'prev,next,today', // Removed space after commas
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,list'
+        },
+        events: '/vacayvue/all_requests/',
+        buttonText: {
+            today: 'Σήμερα',
+            dayGridMonth: 'Μήνας',
+            timeGridWeek: 'Εβδομάδα',
+            list: 'Λίστα'
+        }
+    });
+
+    calendar.render();
+})
